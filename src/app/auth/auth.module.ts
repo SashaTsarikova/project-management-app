8016fcadfab615d1930c1d8ec5f71df46552e3d2
Andrei Ya <36473307+AndreiYa@users.noreply.github.com>
2022-04-27 22:18:07 +0300

    feat: add auth component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import SharedModule from '../shared/shared.module';
+import { AuthComponent } from './auth.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthComponent
+  ],
   imports: [
     SharedModule,
+    RouterModule.forChild([
+      {path: '', component: AuthComponent}
+    ])
   ],
 })
 export class AuthModule { }
